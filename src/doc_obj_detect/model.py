33498898b16db59edc844252f821b1b734d25e89
Lee Miller <dleemiller@protonmail.com>
2025-11-17 20:51:25 -0700

    pruning out legacy scripts

--- src/doc_obj_detect/model.py
+++
@@ -1,6 +0,0 @@
-"""Backward-compatible shims for the new models package."""
-
-from doc_obj_detect.models.builder import ModelFactory, create_model
-from doc_obj_detect.models.info import get_trainable_parameters
-
-__all__ = ["ModelFactory", "create_model", "get_trainable_parameters"]
